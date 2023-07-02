ffb4e3b150b5306bfc1b3c4b74de8a357e2fde91
tatitati <francis.jaa@protonmail.com>
2023-07-02 21:41:18 +0200

    run completed

--- cli/run.sh
+++ cli/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-(cd app && gatsby develop && open localhost:8000)
+PORT_NUMBER=8000
+lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
+(cd app && gatsby develop &)
+
+echo "WAITING TO HAVE GATSBY RUNNING...."
+while ! curl http://localhost:8000/
+do
+  echo "waiting for GATSBY to be running....."
+  sleep 6
+done
+
+open http://localhost:8000/
